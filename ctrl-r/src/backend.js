80c0cad8774ff57ff356457a3475cc218932376f
Aeoli11 <writeaeris@gmail.com>
2026-01-31 14:16:53 -0800

    added file convesion,need to test

--- ctrl-r/src/backend.js
+++ ctrl-r/src/backend.js
@@ -1,23 +1,18 @@
 //once frontend sends file
 import ConvertApi from 'convertapi-js'
 var file;//
+var filetype;
 var conversion;// type to convert into 
 var final;// final product to send back
 
 //for 
 //intial function is to convert wpd to pdf
 //and documents
+//creating the instance of convertApi and adding the file into the convertor
+ let convertApi=ConvertApi.auth('api_token');//put in api token
+let params = convertApi.createParams();
+params.add('File',file);
+let result=await convertApi.convert(filetype,conversion,params);
 
 //additional add ons are :
 // add function for converting lotus to pdf
-function process_wpd_to_pdf(){
-    let convertApi=ConvertApi.auth('api_token');//put in api token
-    let params = convertApi.createParams();
-    params.add('File',)
-
-
-}
-
-function process_wpd_to_doc(){
-
-}
