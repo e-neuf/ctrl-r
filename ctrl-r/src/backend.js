9873c0242530d74223cafdbd4108bb0c2d5b4fb9
Ellie Neufeld <ellie_neufeld@sfu.ca>
2026-01-31 14:59:48 -0800

    convert file and print the url to the console

--- ctrl-r/src/backend.js
+++ ctrl-r/src/backend.js
@@ -1,18 +1,23 @@
 //once frontend sends file
 import ConvertApi from 'convertapi-js'
-var file;//file name
-var filetype;//type of file
-var conversion;// type to convert into 
-var final;// final product to send back
 
-//for 
-//intial function is to convert wpd to pdf
-//and documents
-//creating the instance of convertApi and adding the file into the convertor
- let convertApi=ConvertApi.auth('sLWxDSAaL4XCSIN6Nb4NTVfOQkodGAb4');//put in api token
-let params = convertApi.createParams();
-params.add('File',file);
-let result=await convertApi.convert(filetype,conversion,params);
+
+export const ConvertFile = async (file, currentFiletype, targetFiletype) => {
+
+    //for 
+    //intial function is to convert wpd to pdf
+    //and documents
+    //creating the instance of convertApi and adding the file into the convertor
+
+    let convertApi=ConvertApi.auth('sLWxDSAaL4XCSIN6Nb4NTVfOQkodGAb4');//put in api token
+    let params = convertApi.createParams();
+    params.add('File',file);
+    let result=await convertApi.convert(currentFiletype,targetFiletype,params);
+    let url = result.files[0].Url;
+    console.log(url);
+
+}
+
 
 //additional add ons are :
 // add function for converting lotus to pdf
